2ad15911e75d82198ecd562e3b1c2389ea4be5ed
liamkelly <liamkelly15@gmail.com>
2020-09-26 17:28:08 +0100

    add header tests

--- src/test/java/testbody.java
+++ src/test/java/testbody.java
@@ -38,6 +38,36 @@ public class testbody extends webapiautomation.BaseClass {
         Assert.assertEquals(userID, "pepe@pepe.pe1-0.1");
     }
 
+
+    @Test
+    public void returnsCorrectemail() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/user/all");
+
+        response = client.execute(get);
+
+        String jsonBody = EntityUtils.toString(response.getEntity());
+
+        //System.out.println(jsonBody);
+        String jsonBody2 = jsonBody.substring(1);
+        //System.out.println(jsonBody2);
+        String jsonBody3 = jsonBody2.substring(0, jsonBody2.length() - 1);
+        //System.out.println(jsonBody3);
+
+        JSONObject jsonObject = new JSONObject(jsonBody3);
+
+        //System.out.println(jsonObject);
+
+        String userID = (String) getValueFor(jsonObject, "email");
+
+        System.out.println(userID);
+
+        Assert.assertEquals(userID, "pepe@pepe.pe1");
+    }
+
+
+
+
     private Object getValueFor(JSONObject jsonObject, String key) {
         return jsonObject.get(key);
     }
